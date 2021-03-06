c323af62e936a30293bee757c9370b2089042a99
Paolo D'Incau <paolo.dincau@gmail.com>
2021-03-06 14:55:48 +0100

    extract function

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,10 @@ use crate::structs::{greet, Person};
 mod structs;
 
 fn main() {
+    base_struct_and_function();
+}
+
+fn base_struct_and_function() {
     let me = Person {
         name: "Paolo".to_string(),
         age: 36,
