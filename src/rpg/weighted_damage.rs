82d2457d628878abedc92ec6be6c3cf42f095cc6
Paolo D'Incau <paolo.dincau@gmail.com>
2021-03-14 13:14:59 +0100

    found nasty bug testing component in isolation

--- src/rpg/weighted_damage.rs
+++ src/rpg/weighted_damage.rs
@@ -11,12 +11,28 @@ impl Default for WeightedDamage {
 
 impl WeightedDamage {
     pub fn compute(&self, attacker_level: u16, attackee_level: u16, amount: u16) -> u16 {
-        if attackee_level >= self.factor * attacker_level {
+        if attackee_level >= self.factor + attacker_level {
             amount / 2
-        } else if attacker_level >= self.factor * attackee_level {
+        } else if attacker_level >= self.factor + attackee_level {
             amount * 2
         } else {
             amount
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn when_attacker_level_is_5_above_attackee_damage_is_doubled() {
+        let attacker_level = 10;
+        let attackee_level = 5;
+
+        let weighted_damage =
+            WeightedDamage::default().compute(attacker_level, attackee_level, 100);
+
+        assert_eq!(200, weighted_damage)
     }
 }
